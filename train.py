709b28f157cefe15ba13997524673a0fd57bf87e
Your Personal Name <nimeghalakrishna45@gmail.com>
2026-05-04 01:10:11 +0530

    Trained a model to predict pass/fail on number of study hours and tested

--- train.py
+++ train.py
@@ -1,11 +1,17 @@
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LinearRegression
 import pickle
 import random
 
+
+data = pd.read_csv("data.csv")
+X = data[["hours"]]
+y = data["pass"]
+
 random.seed(45)
-X = np.random.rand(100, 1)
-y = 3 * X + np.random.rand(100, 1)
+# X = np.random.rand(100, 1)
+# y = 3 * X + np.random.rand(100, 1)
 model = LinearRegression()
 model.fit(X, y)
 print("done")
@@ -13,4 +19,4 @@ print("done")
 with open("model.pkl", "wb") as f:
     pickle.dump(model, f)
 
-print("Model trained and saved!")
+print("Model trained and saved on real data!")
