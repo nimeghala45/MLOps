c21e3c021d43011e489a8a121d4fb0bc622da0e5
Your Personal Name <nimeghalakrishna45@gmail.com>
2026-05-03 20:42:46 +0530

    test commit

---
+++ train.py
@@ -0,0 +1,16 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import pickle
+import random
+
+random.seed(45)
+X = np.random.rand(100, 1)
+y = 3 * X + np.random.rand(100, 1)
+model = LinearRegression()
+model.fit(X, y)
+print("done")
+
+with open("model.pkl", "wb") as f:
+    pickle.dump(model, f)
+
+print("Model trained and saved!")
